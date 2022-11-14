00ab1d9b9a4c5570d7f8f8a1b34f636c61441f05
nstill77 <114838536+nstill77@users.noreply.github.com>
2022-11-14 12:25:24 -0800

    Add files via upload
    Chart 1 code on child stunting under 5

---
+++ source/Graph 1.R
@@ -0,0 +1,31 @@
+library("dplyr")
+library("ggmap")
+library("ggplot2")
+
+child_stunting_under_5 <- read.csv(
+  "https://raw.githubusercontent.com/info201a-au2022/project-group-8-section-ag/main/data/share-of-children-younger-than-5-who-suffer-from-stunting.csv")
+
+colnames(child_stunting_under_5)[colnames(child_stunting_under_5) == "Prevalence.of.stunting..height.for.age....of.children.under.5."] ="Prevalence_prcnt"
+
+ggplot(data = child_stunting_under_5) +
+  geom_col(mapping = aes(x = Entity, y = Prevalence_prcnt))
+
+prcnt_per_country <- child_stunting_under_5 %>%
+  group_by(Entity) %>%
+  summarise(sum.prcnt = sum(Prevalence_prcnt))
+
+nrow_per_country <- child_stunting_under_5 %>%
+  group_by(Entity) %>%
+  count(Entity)
+
+avg_stunting_percountry <- left_join(
+  prcnt_per_country,
+  nrow_per_country,
+  by = NULL
+  ) %>%
+  mutate(avg_prevalence = sum.prcnt/n)
+
+colnames(avg_stunting_percountry)[colnames(avg_stunting_percountry) == "Entity"] ="Country"
+
+ggplot(data = avg_stunting_percountry) +
+  geom_col(mapping = aes(x = Country, y = avg_prevalence))
